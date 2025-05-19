9fef4a89bda0e2ce7cfd8b2f0b416bb26950e62a
Devam Tanwani <devamtanwani@gmail.com>
2025-05-19 23:22:30 +0530

    added styles

--- NovelQuest.py
+++ NovelQuest.py
@@ -18,7 +18,7 @@ st.set_page_config(
 )
 
 # Apply custom CSS
-with open("style.css") as f:
+with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Session state initialization
